3a14b0623d9ec7c2f2ca77dc6ebbc7017d8dc956
Richard Kettering <kettering.richard@gmail.com>
2012-06-27 19:57:40 -0500

    Added an implementation of insertion sort.

---
+++ insertionsort.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+
+$items=[]
+
+for num in 1..25
+	$items.push rand(num)
+end
+
+def insertionsort(array)
+  for iter in 1..(array.length-1)
+    moving_num = array[iter]
+    hole_addr = iter
+    while hole_addr > 0 and array[hole_addr - 1] > moving_num
+      array[hole_addr] = array[hole_addr - 1]
+      hole_addr = hole_addr - 1
+    end
+    array[hole_addr] = moving_num
+  end
+end
+
+
+print "$items= ["
+$items.each { |i| print i, ", "}
+print "] \n"
+
+insertionsort($items)
+
+print "$items= ["
+$items.each { |i| print i, ", "}
+print "] \n"
+
